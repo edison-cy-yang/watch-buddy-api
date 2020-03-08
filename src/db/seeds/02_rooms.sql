29a1ccc8118c1cda73303fe655401992d1de6d23
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-08 12:53:29 -0700

    seed file for rooms

---
+++ src/db/seeds/02_rooms.sql
@@ -0,0 +1,6 @@
+INSERT INTO rooms (title, video_url, room_owner)
+VALUES
+('Kobe Bryant highlights', 'https://www.youtube.com/watch?v=OTTWedyp37o', 3),
+('Lebron James highlights', 'https://www.youtube.com/watch?v=b117a8_jALE', 3),
+('React Native Tutorials', 'https://www.youtube.com/watch?v=qSRrxpdMpVc', 1),
+('Persian Caviar in Iran', 'https://www.youtube.com/watch?v=M0DZHOnjuQI', 2);
