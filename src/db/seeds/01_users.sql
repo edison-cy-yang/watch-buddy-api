6e7e0fd2c85ea7ca4c61b1c59ea857b983978483
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-08 12:43:25 -0700

    added user seeds

---
+++ src/db/seeds/01_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, google_id)
+VALUES
+('Edison Yang', 'edison.cy.yang@gmail.com', '101019757614129805122'),
+('Dijia', 'dijiawu@gmail.com', '1234567'),
+('austin', 'acm.liu@gmail.com', '2345678');
