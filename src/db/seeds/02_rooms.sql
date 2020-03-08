7fe059fc11ba9eb78ddfc59e14c65e8cedeedd50
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-08 14:12:53 -0700

    added seeds for relationships

--- src/db/seeds/02_rooms.sql
+++ src/db/seeds/02_rooms.sql
@@ -1,6 +1,7 @@
-INSERT INTO rooms (title, video_url, room_owner)
+INSERT INTO rooms (title, video_url, owner_id)
 VALUES
 ('Kobe Bryant highlights', 'https://www.youtube.com/watch?v=OTTWedyp37o', 3),
 ('Lebron James highlights', 'https://www.youtube.com/watch?v=b117a8_jALE', 3),
 ('React Native Tutorials', 'https://www.youtube.com/watch?v=qSRrxpdMpVc', 1),
-('Persian Caviar in Iran', 'https://www.youtube.com/watch?v=M0DZHOnjuQI', 2);
+('Persian Caviar in Iran', 'https://www.youtube.com/watch?v=M0DZHOnjuQI', 2),
+('Troll', 'https://www.youtube.com/watch?v=oHg5SJYRHA0', 4);
