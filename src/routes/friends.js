337d5e39130effddd76eb149f07c6c35cacc23ae
edison-cy-yang <edison.cy.yang@gmail.com>
2020-03-09 15:43:13 -0700

    route to send friend request

--- src/routes/friends.js
+++ src/routes/friends.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router  = express.Router();
 
+const relationshipsFn = require("../databaseHelpers/relationshipsFn");
+
 module.exports = (db) => {
 
-  router.get("/", (req, res) => {
-    res.json({hi: "world"});
+  router.post("/", async (req, res) => {
+    const currentUserId = req.body.currentUserId;
+    const friendId = req.body.friendId;
+    try {
+      const relationship = await relationshipsFn.sendFriendRequest(db, currentUserId, friendId);
+      res.json(relationship);
+    } catch(err) {
+      res.status(500).json({ error: err.message });
+    }
   });
 
   return router;
